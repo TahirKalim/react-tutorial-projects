5efc4ceb7c3bf2dc36acf033a576c1fa559b6080
tahir kalim <thrkalim@gmail.com>
2021-04-02 17:03:34 +0200

    cocktails project

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+   <section className="section about-section">
+     <h1 className="section-title">about us</h1>
+     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem magnam neque similique ea sit autem mollitia quis sapiente tempore nisi. Eveniet, adipisci hic quis ullam eaque amet nihil praesentium molestias.
+     Molestiae quis blanditiis voluptatibus sit a soluta odio nesciunt voluptas voluptatem alias quae inventore doloribus maiores dicta est temporibus, laudantium quod. Dicta ut vero commodi eos perferendis et ratione voluptatem!
+     </p>
+   </section>
   )
 }
 
