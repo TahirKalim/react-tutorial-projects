874761d22ac7b048fbc96064581a0d34f2434daf
tahir kalim <thrkalim@gmail.com>
2021-04-09 04:50:40 +0200

    pagination

--- 18-pagination/setup/src/Follower.js
+++ 18-pagination/setup/src/Follower.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
-const Follower = () => {
-  return <h2>follower component</h2>
+const Follower = ({avatar_url, html_url, login}) => {
+  return (
+    <article className='card'>
+      <img src={avatar_url} alt={login} className='card' />
+      <h4>{login}</h4>
+    </article>
+  )
 }
 
 export default Follower
