ade8b7b73b4e1056456c058bb53b9e5e87db7eab
tahir kalim <thrkalim@gmail.com>
2021-09-14 20:38:32 +0200

    modification 1

--- 1-birthday-reminder/setup/src/App.js
+++ 1-birthday-reminder/setup/src/App.js
@@ -2,16 +2,22 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  const [people, setPeople] = useState(data)
-  return(
-    <main>
-      <section className='container'>
-        <h3>{people.length} birthdays today</h3>
-        <List people={people} />
-        <button onClick={()=> setPeople([])}>clear all</button>
-      </section>
-    </main>
-  )
+	const [people, setPeople] = useState(data);
+	return (
+		<main>
+			<section className='container'>
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button onClick={() => setPeople([])}>clear all</button>
+			</section>
+		</main>
+	);
 }
-
+//  <main> First step
+//       <section className='container'>
+//         <h3>0 birthdays today</h3>
+//         <List />
+//         <button onClick={()=> console.log('you clicked me')}>clear all</button>
+//       </section>
+//     </main>
 export default App;
--- 9-color-generator/setup/src/App.js
+++ 9-color-generator/setup/src/App.js
@@ -7,32 +7,43 @@ function App() {
   const [color, setColor] = useState('')
   const [error, setError] = useState(false)
   const [list, setList] = useState([])
-  
-  const handleSubmit = (e)=>{
+
+  const handleSubmit = (e) => {
     e.preventDefault()
-    try{
-      let colors = new Values(color).all(10)
-      setList(colors)
-    }catch(error){
-      setError(true)
-      console.log(error)
-    }
+    console.log('hello')
   }
 
-
-return(
-  <>
-  <section className="container" >
-    <h3>color generator</h3>
-    <form onSubmit={handleSubmit} >
-      <input type="text" value={color} onChange={(e)=> setColor(e.target.value)} placeholder="#f15025" />
-      <button className="btn"> submit</button>
-    </form>
-  </section>
-  <section className="colors">
-    <h4>list goes here</h4>
-    </section>
-  </>
-)
+  return (
+    <>
+      <section className='container'>
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#f15025'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          )
+        })}
+      </section>
+    </>
+  )
 }
+
 export default App
