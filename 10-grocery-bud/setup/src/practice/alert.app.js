ab34525d36da4e93ca2aaaa5a220a74d600e78c0
tahir kalim <thrkalim@gmail.com>
2021-10-01 01:48:49 +0200

    practice folder

--- 10-grocery-bud/setup/src/practice/alert.app.js
+++ 10-grocery-bud/setup/src/practice/alert.app.js
@@ -20,7 +20,7 @@ function App() {
     } else if (name && isEditing) {
       // deal with edit
     } else {
-      showAlert(true, 'success', 'item added to the list')
+      // show alert
       const newItem = { id: new Date().getTime().toString(), title: name }
       setList([...list, newItem])
       setName('')
@@ -28,10 +28,6 @@ function App() {
   }
   const showAlert = (show = false, type = '', msg = '') => {
     setAlert({ show, type, msg })
-  }
-  const clearList = () => {
-    showAlert(true, 'danger', 'empty list')
-    setList([])
   }
   return (
     <section className='section-center'>
@@ -54,9 +50,7 @@ function App() {
       {list.length > 0 && (
         <div className='grocery-container'>
           <List items={list} />
-          <button className='clear-btn' onClick={clearList}>
-            clear items
-          </button>
+          <button className='clear-btn'>clear items</button>
         </div>
       )}
     </section>
